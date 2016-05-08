c00b96a65dd019f68ee6f670d956047f8b3fe28b
pttzty <tyzhang@berkeley.edu>
2016-05-08 11:16:58 -0700

    ENH:Adding Bar chart plotting function of strain and cluster

--- mousestyles/visualization/plot_clustering.py
+++ mousestyles/visualization/plot_clustering.py
@@ -53,11 +53,6 @@ def plot_lastp_dist(Z, method, dist, p=10):
         The hierarchical clustering encoded as a linkage matrix
     p: int, optional
         number of merges to plot
-
-    Returns
-    -------
-    Z: ndarray
-        The hierarchical clustering encoded as a linkage matrix.
     """
     last = Z[-p:, 2]
     last_rev = last[::-1]
@@ -77,4 +72,58 @@ def plot_lastp_dist(Z, method, dist, p=10):
         idxs[:-2] + 2, acceleration_rev,
         label='2nd derivative of {} distance'.format(dist))
     plt.legend(handles=[line1, line2])
+    plt.show()
+
+
+def plot_strain_cluster(count_data, groupby_cluster=True):
+    """
+    Plot the side by side bar chart showing the strain distribution
+    of mice in different clusters or the cluster distribution in different
+    strains 
+
+    Parameters
+    ----------
+    count_data: Dictionary
+        A dictionary object with the keys are the cluster numbers and the
+        values are lists containing strain counts
+    groupbu_cluster: Boolean
+        If True, then the barchart is group by cluster otherwise by strain
+    """
+    fig,ax = plt.subplots()
+    n_groups = len(count_data)
+    index = np.arange(n_groups)
+    LABEL_COLOR_MAP = {0 : 'r', 1 : 'b', 2 : 'y',
+                   3 : 'g', 4 : 'chocolate', 5 : 'm',
+                   6 : 'k', 7 : 'gray', 8 : 'maroon',
+                   9 : 'coral', 10 : 'lime', 11 : 'blueviolet',
+                   12 : 'deeppink', 13 : 'steelblue', 14 : 'tan',
+                   15 : 'orange' }
+    strain_names = {0: 'C57BL6J', 1: 'BALB', 2: 'A', 3: '129S1', 4: 'DBA', 
+            5: 'C3H',
+            6: 'AKR', 7: 'SWR', 8: 'SJL', 9: 'FVB', 10: 'WSB', 11: 'CZECH',
+            12: 'CAST', 13: 'JF1', 14: 'MOLF', 15: 'SPRET'}
+    bar_width = 1 / len(count_data[0])
+    opacity = 0.4
+    for i in range(len(count_data[0])):
+        sub_count_data = [t[i] for t in count_data.values()]
+        plt.bar(
+        index + i * bar_width, sub_count_data, bar_width,
+        alpha=opacity,
+        color=LABEL_COLOR_MAP[i],
+        label=strain_names[i])
+    if groupby_cluster == True:
+        plt.xlabel('Clusters')
+        plt.title('Counts by cluster and strain')
+    else:
+        plt.xlabel('Strains')
+        lt.title('Counts by strain and cluster')
+    plt.ylabel('Counts')
+    plt.xticks(
+        (index + 1) * len(count_data[0]) * bar_width - 0.5, 
+        ['Cluster' + str(t) for t in range(1,n_groups + 1)])
+    for x in range(1, n_groups + 1):
+        plt.axvline(x=x, linestyle='--')
+    ax.set_xlim([0.0, n_groups + 1])
+    plt.legend(loc='right')
+    plt.tight_layout()
     plt.show()
