60e4f7b070faefc884950e527b292095021a7836
Chih-Hui Wang <jjason88105@gmail.com>
2016-05-09 22:01:58 -0700

    TST: create the fake data and change the tests

--- mousestyles/path_diversity/tests/test_filter_path.py
+++ mousestyles/path_diversity/tests/test_filter_path.py
@@ -2,29 +2,38 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import pytest
+import pandas as pd
 
 from mousestyles import data
-from mousestyles.path_diversity import path_index
-from mousestyles.path_diversity import filter_path
+from mousestyles import path_diversity
 
 
 def test_filter_path_input():
     movement = data.load_movement(0, 0, 0)
-    paths = path_index(movement, 1, 1)
+    paths = path_diversity.path_index(movement, 1, 1)
     # checking functions raise the correct errors
     # input negative number
     with pytest.raises(ValueError) as excinfo:
-        filter_path.filter_paths(movement, paths, -1)
+        path_diversity.filter_paths(movement, paths, -1)
     assert excinfo.value.args[0] == "Input values need to be positive"
     # input zeros
     with pytest.raises(ValueError) as excinfo:
-        filter_path.filter_paths(movement, paths, 0)
+        path_diversity.filter_paths(movement, paths, 0)
     assert excinfo.value.args[0] == "Input values need to be positive"
 
 
 def test_filter_path():
-    movement = data.load_movement(0, 0, 0)
-    paths = path_index(movement, 1, 1)
+    movement = {
+        't': pd.Series([0.5, 0.6, 1.0, 2.5, 2.6, 2.7,
+                        4.0, 4.5, 5.0, 6.0, 8.0, 8.5, 10.0]),
+        'x': pd.Series([1.42, 1.96, 1.79, 1.85, 1.80, 1.98,
+                        1.19, 1.30, 1.85, 1.86, 1.90, 1.76, 1.65]),
+        'y': pd.Series([1.67, 1.60, 1.13, 1.95, 1.65, 1.11,
+                        1.71, 1.37, 1.88, 1.97, 1.11, 1.15, 1.38])
+    }
+
+    movement = pd.DataFrame(movement)
+    paths = path_diversity.path_index(movement, 1, 1)
     # Checking functions output the correct path
-    pass_paths = filter_path.filter_paths(movement, paths, 20)
-    assert pass_paths == [[3082, 3181], [30835, 30970], [31346, 31557]]
+    pass_paths = path_diversity.filter_paths(movement, paths, 1)
+    assert pass_paths == [[6, 8]]
