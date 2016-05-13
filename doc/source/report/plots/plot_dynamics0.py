89771baa32aaf5e792df4f314728c14d4ee68382
Mingyung Kim <mingyung_kim@berkeley.edu>
2016-05-13 13:16:59 -0700

    DOC: add dynamic plots for adding to the report

---
+++ doc/source/report/plots/plot_dynamics0.py
@@ -0,0 +1,6 @@
+from mousestyles.data import load_time_matrix_dynamics
+from mousestyles.visualization.dynamics import plot_dynamics
+
+
+time_matrix = load_time_matrix_dynamics()
+plot_dynamics(time_matrix, 0)
