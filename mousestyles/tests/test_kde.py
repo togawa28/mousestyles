0c5d581f8f8d614aaa44fdf948f24393e4091ecd
Fengshi Niu <fniu@airbears2-10-142-116-199.airbears2.1918.berkeley.edu>
2016-05-13 16:24:57 -0700

    test for kde function

---
+++ mousestyles/tests/test_kde.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import numpy as np
+from scipy.stats.distributions import norm
+from mousestyles.kde import kde
+
+def test_kde():
+    pdf = kde(x = np.array([2,3,1,0]), x_grid=np.linspace(0, 5, 10))
+    assert (type(pdf) == np.ndarray)
+    assert all([item >= 0 for item in pdf])
+    assert (len(pdf) == 10)
+    x1 = np.concatenate([norm(-1, 1.).rvs(400),
+                    norm(1, 0.3).rvs(100)])
+    pdf = kde(x = x1)
+    assert (type(pdf) == np.ndarray)
+    assert all([item >= 0 for item in pdf])
+    assert (len(pdf) == 300)
