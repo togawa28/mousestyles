d7fa4a2dd5a0e0f0921dbec51e0d98bbdc5338c4
Mengfei Jiang <mengfei.jiang@berkeley.edu>
2016-05-13 10:00:50 -0700

    BUG: fix the data loading bug for generating plots in report

--- doc/source/report/plots/plot_svm_result.py
+++ doc/source/report/plots/plot_svm_result.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
+import os.path as _osp
+from mousestyles import pkg_dir
 from mousestyles.visualization import plot_classification
 
 
 # load data
-SVM_result = pd.DataFrame(np.load('SVM_result.npy'))
+SVM_result = pd.DataFrame(np.load(
+                            _osp.join(
+                                pkg_dir, '..', 'doc', 'source', 'report', 'plots',
+                                'SVM_result.npy')))
 # plot
 plot_classification.plot_performance(SVM_result)
