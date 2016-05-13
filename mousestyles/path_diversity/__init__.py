ecdfc299f076d21fd02c60f91cca8a7f84aab3af
Chih-Hui Wang <jjason88105@gmail.com>
2016-05-13 09:40:46 -0700

    ENH: add detect_noise and smooth_noise to __init__.py

--- mousestyles/path_diversity/__init__.py
+++ mousestyles/path_diversity/__init__.py
@@ -2,8 +2,10 @@ from __future__ import print_function, absolute_import, division
 
 from mousestyles import data
 from .path_index import path_index
-from .filter_path import filter_paths
+from .filter_path import filter_path
 from .get_dist_speed import get_dist_speed
 from .path_features import (compute_accelerations, compute_angles,
                             angle_between)
 from .clean_movements import clean_movements
+from .detect_noise import detect_noise
+from .smooth_noise import smooth_noise
