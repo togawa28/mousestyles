34dd7c66310b29c954609f7a4acd4d6b2a97a347
Fengshi Niu <fniu@Fengshi-MacBook.local>
2016-05-13 20:48:01 -0700

    STY: add comment explaining the purpose of test

--- mousestyles/tests/test_kde.py
+++ mousestyles/tests/test_kde.py
@@ -5,12 +5,21 @@ from mousestyles.kde import kde
 
 def test_kde():
     pdf = kde(x=np.array([2, 3, 1, 0]), x_grid=np.linspace(0, 5, 10))
+    # check function kde returns an np.ndarray
     assert (type(pdf) == np.ndarray)
+    # check function kde returns array with all nonnegative numbers
     assert all([item >= 0 for item in pdf])
+    # check function kde returns array with length the same as length of x_grid
     assert (len(pdf) == 10)
+
     x1 = np.concatenate([norm(-1, 1.).rvs(400),
                         norm(1, 0.3).rvs(100)])
+    # check kde works fine for a more complicated input, and the default value
+    # of x_grid works fine
     pdf = kde(x=x1)
+    # check function kde returns an np.ndarray
     assert (type(pdf) == np.ndarray)
+    # check function kde returns array with all nonnegative numbers
     assert all([item >= 0 for item in pdf])
+    # check function kde returns array with length the same as length of x_grid
     assert (len(pdf) == 300)
