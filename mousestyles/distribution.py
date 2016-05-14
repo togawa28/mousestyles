0cf9431916c351574aba38bdcc3c7fc0be56047a
Fengshi Niu <fniu@ucbvpn-209-142.vpn.berkeley.edu>
2016-05-14 02:53:44 -0700

    ENH, TST: inverse cdf of power law and truncated exponential

--- mousestyles/distribution.py
+++ mousestyles/distribution.py
@@ -8,14 +8,14 @@ def powerlaw_pdf(x, a):
 
     Parameters
     ----------
-    x : int
+    x : float > 0, or a np.dnarray
         x in formula p(x)=(alpha-1)*x^(-alpha).
-    a : int>1
+    a : float > 1
         alpha in formula p(x)=(alpha-1)*x^(-alpha).
 
     Returns
     -------
-    probability density : a float number
+    probability density : float
         The probability density of power law at x.
 
     Examples
@@ -23,7 +23,7 @@ def powerlaw_pdf(x, a):
     >>> powerlaw_pdf (2, 2)
     0.25
     """
-    return((a - 1) * x ** (-a))
+    return (a - 1) * x ** (-a)
 
 
 def exp_pdf(x, l):
@@ -32,14 +32,14 @@ def exp_pdf(x, l):
 
     Parameters
     ----------
-    x : int
+    x : float, or a np.dnarray
         x in formula p(x)=lambda*exp(-lambda*x).
-    l : int
+    l : float
         lambda in formula p(x)=lambda*exp(-lambda*x).
 
     Returns
     -------
-    probability density : a float number
+    probability density : float
         The probability density of power law at x.
 
     Examples
@@ -47,4 +47,54 @@ def exp_pdf(x, l):
     >>> exp_pdf(1, 1)
     0.36787944117144233
     """
-    return(l * np.exp(-l * (x - 1)))
+    return l * np.exp(-l * (x - 1))
+
+
+def powerlaw_inverse_cdf(y, a):
+    """
+    The inverse CDF function of power law distribution
+
+    Parameters
+    ----------
+    y : float in [0, 1], or a np.dnarray
+        y in formula F^(-1)(y) = (1 - y)^(1/(1-a))
+    a : float > 1
+        a in formula F^(-1)(y) = (1 - y)^(1/(1-a))
+
+    Returns
+    -------
+    x : float
+        The inverse CDF function of power law distribution with
+        parameter a at point y
+
+    Examples
+    --------
+    >>> powerlaw_inverse_cdf(0.5, 5)
+    1.189207115002721
+    """
+    return (1 - y)**(1/(1-a))
+
+
+def exp_inverse_cdf(y, l):
+    """
+    The inverse CDF function of truncated (at 1) exponential distribution
+
+    Parameters
+    ----------
+    y : float in [0, 1], or a np.dnarray
+        y in formula F^(-1)(y) = 1 - log(1 - y) / l
+    l : float > 0
+        a in formula F^(-1)(y) = 1 - log(1 - y) / l
+
+    Returns
+    -------
+    x : float
+        The inverse CDF function of truncated (at 1) exponential distribution
+        distribution with parameter l at point y
+
+    Examples
+    --------
+    >>> exp_inverse_cdf(0.6,2)
+    1.4581453659370776
+    """
+    return 1 - np.log(1 - y) / l
--- mousestyles/tests/test_distribution.py
+++ mousestyles/tests/test_distribution.py
@@ -1,5 +1,5 @@
-# coding: utf-8
-from mousestyles.distribution import (powerlaw_pdf, exp_pdf)
+from mousestyles.distribution import (powerlaw_pdf, exp_pdf,
+                                      powerlaw_inverse_cdf, exp_inverse_cdf)
 
 
 def test_powerlaw_pdf():
@@ -8,3 +8,19 @@ def test_powerlaw_pdf():
 
 def test_exp_pdf():
     assert (exp_pdf(2, 1) == 0.36787944117144233)
+
+
+def test_powerlaw_inverse_cdf():
+    y = np.arange(0, 1, 0.01)
+    x = powerlaw_inverse_cdf(y, 2)
+    assert (type(x) == np.ndarray)
+    assert (all([item >= 1 for item in x]))
+    assert (x[0] == 1)
+
+
+def test_exp_inverse_cdf():
+    y = np.arange(0, 1, 0.01)
+    x = exp_inverse_cdf(y, 2)
+    assert (type(x) == np.ndarray)
+    assert (all([item >= 1 for item in x]))
+    assert (x[0] == 1)
